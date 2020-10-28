175be76cd23b6044324297b7235e89429b61c7bf
emadam <ldy9037@naver.com>
2020-10-28 22:58:06 +0900

    - statement.js 내에 htmlStatement 함수와 renderHtml함수 추가  - usd함수 최상위로 추출  - htmlStatement함수를 이용해 index.html에 영수증 출력

--- example_1/statement.js
+++ example_1/statement.js
@@ -16,10 +16,29 @@ function renderPlainText(data) {
     result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
     return result;
 
-    function usd(aNumber){
-        return new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2}).format(aNumber/100)
-    }
 
+
+}
+
+function htmlStatement(invoice, plays) {
+    return readerHtml(createStatementData(invoice, plays));
+}
+
+function readerHtml(data){
+    let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+    for (let perf of data.performances) {
+        result += ` <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td><td>${usd(perf.amount)}</td></tr>`;
+    }
+    result += "</table>\n";
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+    return result;
+}
+
+function usd(aNumber){
+    return new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2}).format(aNumber/100)
 }
 
 const plays = {
@@ -46,4 +65,5 @@ const invoices = {
     ]
 };
 
-console.log(statement(invoices, plays))
+
+document.getElementsByClassName('render-container')[0].innerHTML = htmlStatement(invoices, plays);
