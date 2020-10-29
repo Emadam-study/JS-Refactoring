d535edd46f454ad789d03f2fb55e499cd0a4cd29
emadam <ldy9037@naver.com>
2020-10-29 21:35:39 +0900

    - enrichPerformance 함수내 play와 amount값을 PerformanceCalculator를 통해 가져오도록 변경

--- example_1/createStatementData.js
+++ example_1/createStatementData.js
@@ -40,8 +40,8 @@ export default function createStatementData(invoice, plays){
     function enrichPerformance(aPerformance){
         const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
-        result.amount = amountFor(result);
+        result.play = calculator.play;
+        result.amount = calculator.amount;
         result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
